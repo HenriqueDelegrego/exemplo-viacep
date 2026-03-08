822626ff7753c55c9b7ccbc4c04b2197f91bf567
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-08 18:04:44 -0300

    export api request to another file

---
+++ src/viacep/api.js
@@ -0,0 +1,21 @@
+async function obterEndereco(cep) {
+
+    // Faz a requisição para a API ViaCEP usando o valor do CEP
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+    // Verifica se houve erro HTTP na requisição (ex: 404, 500)
+    if (!response.ok) {
+        throw new Error("Falha ao buscar endereço");
+    }
+
+    // Converte o corpo da resposta para JSON e retorna um objeto JavaScript
+    const data = await response.json();
+
+    // Se a API retornar o atributo erro (algo específico da ViaCEP), lança um erro
+    if (data.erro) {
+        throw new Error('CEP não encontrado');
+    }
+
+    // Retorna os dados do endereço obtidos da API
+    return data;
+}
--- src/viacep/script.js
+++ src/viacep/script.js
@@ -6,37 +6,32 @@ const cidadeInput = document.getElementById('cidade');
 const ufInput = document.getElementById('uf');
 
 // Adiciona um event listener para o campo de CEP, disparando a cada alteração (input)
-cepInput.addEventListener('input', () => {
+cepInput.addEventListener('input', async (e) => {
+    e.preventDefault();
 
     cepInput.value = cepInput.value.replace(/\D/g, ''); // Remove tudo que não for dígito
 
-    let cep = cepInput.value;
+    const cep = cepInput.value;
 
     if (cep.length !== 8) {
         limparCampos();
         return;
     }
 
-    // Faz a requisição para a API ViaCEP usando o valor do CEP
-    fetch(`https://viacep.com.br/ws/${cep}/json/`) // Retorna uma Promise
-        .then(response => response.json()) // A Promise é resolvida com a resposta da API e convertida para JSON
-        .then(data => {
-            // Se a API retornar o atributo erro (algo específico da ViaCEP), lança um erro
-            if (data.erro) {
-                throw new Error('CEP não encontrado');
-            }
-            // Preenche os campos do formulário com os dados retornados pela API
-            logradouroInput.value = data.logradouro;
-            bairroInput.value = data.bairro;
-            cidadeInput.value = data.localidade;
-            ufInput.value = data.uf;
-        })
-        .catch(error => {
-            // Em caso de erro (CEP não encontrado ou problema na requisição), exibe um alerta e limpa os campos
-            alert('Erro: ' + error.message);
-            limparCampos();
-        });
+    try {
+        const endereco = await obterEndereco(cep);
+
+        logradouroInput.value = endereco.logradouro;
+        bairroInput.value = endereco.bairro;
+        cidadeInput.value = endereco.localidade;
+        ufInput.value = endereco.uf;
+    }
+    catch (error) {
+        alert('Erro: ' + error.message);
+    }
+
 });
+
 
 // Função para limpar os campos menos o CEP do formulário
 function limparCampos() {
